f4599d708a1a9fc084adc40c6dfb0b7c056f2950
Ryan Dorothy <ryan.dorothy@mlh0bzfft3.rackspace.corp>
2017-01-10 13:45:02 -0600

    modified the conf-gen.py script to take in parameters in support of Jenkins and modified the file locations to support the new variable schema being deployed.

--- conf-gen.py
+++ conf-gen.py
@@ -1,36 +1,42 @@
+import argparse
 import subprocess
 
 
-def get_external_lb_vip():
-    host = subprocess.check_output(["hostname"])
-    if "qe-iad3-3" in host:
-        h = "qe-iad3-lab03"
-        fi = "/opt/rpc-openstack/jenkins-oa/inventory/group_vars/{0}.yml".format(h)
-    elif "qe-iad3-2" in host:
-        fi = "/etc/openstack_deploy/openstack_user_config.yml"
-    else:
-        raise Exception("conf-gen.py is for iad3-2 and iad3-3.")
-
-    elva = subprocess.check_output([
-        "grep", "-R", "external_lb_vip_address", fi])
+def get_external_lb_vip(file_location=None):
+    file_location = (file_location
+                     or "/etc/openstack_deploy/openstack_user_config.yml")
+    elva_line = subprocess.check_output([
+        "grep", "-R", "external_lb_vip_address", file_location])
+    elva = elva_line.split(":", 1)[1].strip()
     return elva
 
 
-def get_password():
-    password = subprocess.check_output([
-        "grep", "-R", "kibana_password",
-        "/etc/openstack_deploy/user_extras_secrets.yml"])
+def get_password(file_location=None):
+    file_location = (file_location
+                     or "/etc/openstack_deploy/user_osa_secrets.yml")
+    password_line = subprocess.check_output([
+        "grep", "-R", "keystone_auth_admin_password", file_location])
+    password = password_line.split(":", 1)[1].strip()
     return password
 
-vip = get_external_lb_vip()
-pas = get_password()
+if __name__ == '__main__':
+    description = "Script to generate config file"
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--password-file")
+    parser.add_argument("--vip-file")
+    parser.add_argument("--password")
+    parser.add_argument("--vip")
+    args = parser.parse_args()
 
-f = open('/opt/horizon-selenium/config/app.yaml', 'w')
-f.write("horizon:\n")
-f.close()
-append = open('/opt/horizon-selenium/config/app.yaml', 'a')
-append.write("  username: horizon\n")
-append.write("  "+pas)
-append.write(vip)
-append.close()
-print "Config file generated into config/app.yaml"
+    vip = args.vip or get_external_lb_vip(args.vip_file)
+    pas = args.password or get_password(args.password_file)
+
+    f = open('/opt/horizon-selenium/config/app.yaml', 'w+')
+    f.write("horizon:\n")
+    f.close()
+    append = open('/opt/horizon-selenium/config/app.yaml', 'a')
+    append.write("  username: admin\n")
+    append.write("  horizon_password: {0}\n".format(pas))
+    append.write("  external_lb_vip_address: {0}".format(vip))
+    append.close()
+    print "Config file generated into config/app.yaml"
